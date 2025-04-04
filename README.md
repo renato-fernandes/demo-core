c9514fb9c4b5f37ad9e1e303f007c22cad214299
Renato Fernandes <renato.fernandes.sorocaba@gmail.com>
2025-04-04 14:56:03 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Swagger UI:
+
+Local (Dev environment):
+http://localhost:8080/swagger-ui/index.html
---
+++ src/main/java/com/rf/sorocaba/demo/core/controller/UsersController.java
@@ -0,0 +1,8 @@
+package com.rf.sorocaba.demo.core.controller;
+
+import com.rf.sorocaba.demo.core.api.UsersApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController implements UsersApi {
+}
